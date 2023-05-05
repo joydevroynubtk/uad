1cdad7999550de7e0ed5551c9245865acb8d8ec7
uad <joydevroynubtk@gmail.com>
2023-05-05 15:01:12 +0600

    commit-two

--- lib/screens/my_profile/my_profile.dart
+++ lib/screens/my_profile/my_profile.dart
@@ -73,50 +73,142 @@ class MyProfileScreen extends StatelessWidget {
               ),
             ),
             sizedBox,
-            Container(
-              padding: EdgeInsets.only(right: kDefaultPadding / 4, left: kDefaultPadding / 4,
-              top: kDefaultPadding / 2),
-              width: MediaQuery.of(context).size.width / 2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Registration Number',
-                        style:
-                            Theme.of(context).textTheme.titleMedium!.copyWith(
-                                  color: kTextLightColor,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15.0,
-                                ),
-                      ),
-                      Text(
-                        '11200120554',
-                        style:
-                            Theme.of(context).textTheme.titleMedium!.copyWith(
-                                  color: kTextBlackColor,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15.0,
-                                ),
-                      ),
-                      kHalfSizedBox,
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width / 3,
-                        child: Divider(
-                          thickness: 1.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Icon(Icons.lock_outline,size: 20.0,),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ProfileDetailRow(
+                    title: 'Registration Number', value: '11200120554'),
+                ProfileDetailRow(title: 'Academic Year', value: '2020-2023'),
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ProfileDetailRow(title: 'Admission Class', value: 'X-11'),
+                ProfileDetailRow(title: 'Admission Number', value: '000126'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ProfileDetailRow(
+                    title: 'Date of Admission', value: '1 Jan, 2020'),
+                ProfileDetailRow(title: 'Date of Birth', value: '26 Nov, 1998'),
+              ],
+            ),
+            ProfileDetailColumn(title: 'Email', value: 'joydevroynubtk@gmail.com',),
+            ProfileDetailColumn(title: 'Father Name', value: 'Bishwajit Roy',),
+            ProfileDetailColumn(title: 'Mother Name', value: 'Loxmi Roy',),
+            ProfileDetailColumn(title: 'Phone Number', value: '+8801753607004',),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ProfileDetailRow extends StatelessWidget {
+  const ProfileDetailRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+  final String title;
+  final String value;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          right: kDefaultPadding / 4,
+          left: kDefaultPadding / 4,
+          top: kDefaultPadding / 2),
+      width: MediaQuery.of(context).size.width / 2,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: kTextLightColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15.0,
+                    ),
+              ),
+              Text(
+                value,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: kTextBlackColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15.0,
+                    ),
+              ),
+              kHalfSizedBox,
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                child: Divider(
+                  thickness: 1.0,
+                ),
+              ),
+            ],
+          ),
+          Icon(
+            Icons.lock_outline,
+            size: 20.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileDetailColumn extends StatelessWidget {
+  const ProfileDetailColumn(
+      {Key? key, required this.title, required this.value})
+      : super(key: key);
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: kTextLightColor,
+                      fontSize: 15.0,
+                    ),
+              ),
+              kHalfSizedBox,
+              Text(
+                value,
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: kTextBlackColor,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+              kHalfSizedBox,
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: Divider(
+                  thickness: 1.0,
+                ),
+              )
+            ],
+          ),
+          Icon(
+            Icons.lock_outline,
+            size: 20.0,
+          ),
+        ],
       ),
     );
   }
